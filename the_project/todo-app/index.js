010bd923d2fb58b5a132497cbc0666f3e3d0b96e
ratilmii <ratilmii@gmail.com>
2026-04-24 16:27:34 +0300

    add html page get request and env variable for port

--- the_project/todo-app/index.js
+++ the_project/todo-app/index.js
@@ -3,7 +3,18 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send("Todo app server running")
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Todo App</title>
+      </head>
+      <body>
+        <h1>Todo app server running.</h1>
+        <p>Welcome!</p>
+      </body>
+    </html>
+  `)
 })
 
 app.listen(port, () => {

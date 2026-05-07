b1262be8834b5a55c5730626c39492abd6f074b8
ratilmii <ratilmii@gmail.com>
2026-05-07 14:49:04 +0300

    add ingress to log-output and edit index.js to work in browser

--- log_output/index.js
+++ log_output/index.js
@@ -1,8 +1,39 @@
+const express = require("express");
 const { randomUUID } = require("crypto");
+
+const app = express();
+const port = process.env.PORT || 8060;
 
 const id = randomUUID();
 
+function getStatus() {
+  return {
+    timestamp: new Date().toISOString(),
+    id,
+  };
+}
+
 setInterval(() => {
-  const timestamp = new Date().toISOString();  
-  console.log(`${timestamp}: ${id}`);
+  const status = getStatus();
+  console.log(`${status.timestamp}: ${status.id}`);
 }, 5000);
+
+app.get('/', (req, res) => {
+  const status = getStatus();
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Log Output</title>
+      </head>
+      <body>
+        <h1>Log Output Status</h1>
+        <p>${status.timestamp}: ${status.id}</p>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}`);
+});
